60636c58634630bbeee1dec195805d7275078cff
Alice Gathigia <muraguria537@gmail.com>
2026-05-18 19:49:41 +0300

    clock app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
-import Navbar from "./components/Navbar";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import './App.css'
+import { useEffect, useState } from "react";
+import { format } from "date-fns";
 
-function App(){
-	return(
-		<>
-		<Navbar/>
-		<MainContent/>
-		<Footer/>
-		</>
-	)
+function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div style={styles.container}>
+      <h1>Dynamic Color Clock</h1>
+
+      <div style={styles.clock}>
+        {format(time, "EEEE, MMMM do yyyy")}
+        <br />
+        {format(time, "hh:mm:ss a")}
+      </div>
+    </div>
+  );
 }
-export default App
+
+const styles = {
+  container: {
+    textAlign: "center",
+    marginTop: "100px",
+    fontFamily: "Arial",
+  },
+
+  clock: {
+    fontSize: "2rem",
+    color: "white",
+    backgroundColor: "purple",
+    padding: "20px",
+    borderRadius: "15px",
+    display: "inline-block",
+  },
+};
+
+export default App;
